b37c9a4830690e108f90a35ff729fb4c3ad408d1
Angela Stephens <angelastephens317@gmail.com>
2020-06-01 01:48:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n>0
+  puts "#{n} SECOND(S)!"
+  n -=1
+  end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  while n>=5
+  puts sleep (1)
+  n -=5
+  end
 end
